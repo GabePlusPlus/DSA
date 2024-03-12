f56cae638f5db13c01908486bd756a00525218cc
GabePlusPlus <grgu@proton.me>
2024-03-12 13:02:28 +0000

    Convert raw to smart pointers in Stack.hpp

--- Stack/Stack.hpp
+++ Stack/Stack.hpp
@@ -1,55 +1,49 @@
 /*
  * TODO:
- *    - Replace raw pointers with smart pointers
- *    - Implement a peek function
+ *  - Implement peek method
  */
 
 #pragma once
 
+#include <memory>
 #include <stdexcept>
+#include <utility>
 
 template <typename T>
 class StackItem {
 private:
     T value;
-    StackItem* below;
+    std::unique_ptr<StackItem> below;
 
 public:
-    StackItem(T const value = T(), StackItem* const below = nullptr);
+    StackItem(T const value = T(),
+        std::unique_ptr<StackItem>&& const below = nullptr);
     T getValue() const;
-    StackItem* getBelow() const;
-    void setValue(T const value);
-    void setBelow(StackItem* const below);
+    std::unique_ptr<StackItem> getBelow();
 };
 
 template <typename T>
 class Stack {
 private:
-    StackItem<T>* top;
+    std::unique_ptr<StackItem<T>> top;
 
 public:
     Stack();
-    ~Stack();
     void push(T const value);
     T pop();
-    void clear();
     bool isEmpty() const;
 };
 
 /*
- * STACKITEM
- */
+* STACKITEM
+*/
 
 template <typename T>
-inline StackItem<T>::StackItem(T const value /* = T() */,
-    StackItem* const below /* = nullptr */) {
+inline StackItem<T>::StackItem(
+    T const value /* = T() */,
+    std::unique_ptr<StackItem>&& const below /* = nullptr */) {
     this->value = value;
-    this->below = below;
-}
-
-template <typename T>
-inline StackItem<T>* StackItem<T>::getBelow() const {
-    return below;
+    this->below = std::move(below);
 }
 
 template <typename T>
@@ -58,18 +52,13 @@ inline T StackItem<T>::getValue() const {
 }
 
 template <typename T>
-inline void StackItem<T>::setBelow(StackItem* const below) {
-    this->below = below;
-}
-
-template <typename T>
-inline void StackItem<T>::setValue(T const value) {
-    this->value = value;
+inline std::unique_ptr<StackItem<T>> StackItem<T>::getBelow() {
+    return std::move(below);
 }
 
 /*
- * STACK
- */
+* STACK
+*/
 
 template <typename T>
 Stack<T>::Stack() : top(nullptr) {}
@@ -77,48 +66,30 @@ Stack<T>::Stack() : top(nullptr) {}
 #ifdef _MSC_VER
 template <typename T>
 __forceinline bool Stack<T>::isEmpty() const {
-    return top == nullptr ? true : false;
+    return !top;
 }
 #else
 template <typename T>
 inline bool Stack<T>::isEmpty() const {
-    return top == nullptr ? true : false;
+    return !top;
 }
 #endif
 
 template <typename T>
-Stack<T>::~Stack() {
-    if (!isEmpty()) clear();
-}
-
-template <typename T>
 void Stack<T>::push(T const value) {
-    StackItem<T>* const new_item = new StackItem<T>(value);
-    if (!isEmpty()) new_item->setBelow(top);
-    top = new_item;
+    if (!isEmpty())
+        top = std::make_unique<StackItem<T>>(value, std::move(top));
+    else
+        top = std::make_unique<StackItem<T>>(value);
 }
 
 template <typename T>
 T Stack<T>::pop() {
     if (!isEmpty()) {
         T const ret = top->getValue();
-        StackItem<T>* temp = top;
         top = top->getBelow();
-        delete temp;
         return ret;
     } else {
         throw std::out_of_range("pop: stack is empty");
-    }
-}
-
-template <typename T>
-void Stack<T>::clear() {
-    if (!isEmpty()) {
-        StackItem<T>* temp = top;
-        while (temp != nullptr) {
-            top = top->getBelow();
-            delete temp;
-            temp = top;
-        }
     }
 }
