533ea5ec136b522e5aa73e27ccf0e8b651e681f1
GabePlusPlus <grgu@proton.me>
2024-03-28 15:05:59 +0000

    Add descriptive comments in Digraph.hpp

--- Digraph/Digraph.hpp
+++ Digraph/Digraph.hpp
@@ -1,6 +1,7 @@
 /*
-* TODO:
-*  - Document
+* A digraph (directed graph) implementation where each node is uniquely
+* identified by a UID (unique identifier). Designed for use with arithmetic
+* types; delete line 57 for use with other types (functionality not guaranteed).
 */
 
 #pragma once
@@ -21,8 +22,10 @@ template <typename T>
 class Node {
 private:
     T uid;
-    std::unordered_set<T> in;
-    std::unordered_set<T> out;
+    std::unordered_set<T> in; // contains in-neighbour UIDs
+                              // (in-neighbour -> node)
+    std::unordered_set<T> out; // contains out-neighbour UIDs
+                               // (node -> out-neighbour)
 
     template <typename T>
     friend class Digraph;
@@ -55,6 +58,8 @@ class Digraph {
 
 private:
     std::map<T, std::unique_ptr<Node<T>>> nodes;
+    //       ^  ^^^^^^^^^^^^^^^^^^^^^^^^
+    //     UID  pointer to node with matching UID
 
     std::map<T, std::unique_ptr<Node<T>>>::iterator getNode(T const uid,
         std::string const caller);
@@ -98,6 +103,11 @@ inline
 #endif
 std::map<T, std::unique_ptr<Node<T>>>::iterator Digraph<T>::getNode(
     T const uid, std::string const caller) {
+    /*
+     * Returns an iterator `itr` associated with Digraph<T>::nodes
+     * where itr->second points to the node with a UID equal to the
+     * `uid` argument (or throws an exception if no such node exists)
+     */
     auto const itr = nodes.find(uid);
     if (itr == nodes.end()) throw node_not_found(caller, uid);
     return itr;
@@ -106,16 +116,24 @@ std::map<T, std::unique_ptr<Node<T>>>::iterator Digraph<T>::getNode(
 template <typename T>
 inline void Digraph<T>::addNode(T const uid) {
     if (nodes.find(uid) == nodes.end())
+        // node with matching UID does not exist--proceed
         nodes[uid] = std::unique_ptr<Node<T>>(new Node<T>(uid));
     else
+        // node with matching UID exists--abort
         throw node_uniqueness_violation(uid);
 }
 
 template <typename T>
 inline void Digraph<T>::deleteNode(T const uid) {
     auto const to_delete = getNode(uid, FUNC_NAME);
+    // erase the deleted node's UID from its in-neighbours'
+    // out-neighbour set ("we are no longer connected
+    // to this node")
     for (auto const in_neighbour : to_delete->second->in)
         getNode(in_neighbour, FUNC_NAME)->second->out.erase(uid);
+    // erase the deleted node's UID from its out-neighbours'
+    // in-neighbour set ("this node is no longer connected
+    // to us")
     for (auto const out_neighbour : to_delete->second->out)
         getNode(out_neighbour, FUNC_NAME)->second->in.erase(uid);
     nodes.erase(to_delete);
@@ -157,12 +175,15 @@ inline std::size_t Digraph<T>::inDegree(T const uid) {
 
 template <typename T>
 void Digraph<T>::bfSearch(T const root_uid, std::vector<T>& const vec) {
-    std::unordered_set<T> visited;
+    std::unordered_set<T> visited; // prevents cycle loops
     vec.push_back(root_uid);
     visited.insert(root_uid);
     for (std::size_t index = 0; index != vec.size(); index++) {
+    // vec.size() will increase until all nodes have been visited
         auto curr_uid = *(vec.begin() + index);
         auto curr_node = getNode(curr_uid, FUNC_NAME);
+        // push out-neighbours to vec
+        // they will be visited over subsequent for-loop iterations
         for (auto out_neighbour : curr_node->second->out) {
             if (visited.find(out_neighbour) == visited.end()) {
                 vec.push_back(out_neighbour);
@@ -174,14 +195,15 @@ void Digraph<T>::bfSearch(T const root_uid, std::vector<T>& const vec) {
 
 template <typename T>
 void Digraph<T>::dfSearch(T const root_uid, std::vector<T>& const vec) {
-    std::unordered_set<T> traversed;
+    std::unordered_set<T> traversed; // prevents cycle loops
     std::function<void(T)> dfs = [&](T const curr_uid) {
         traversed.insert(curr_uid);
         auto curr_node = getNode(curr_uid, FUNC_NAME);
+        // iterate through out-neighbours before...
         for (auto out_neighbour : curr_node->second->out)
             if (traversed.find(out_neighbour) == traversed.end())
                 dfs(out_neighbour);
-        vec.push_back(curr_uid);
+        vec.push_back(curr_uid); // ...visiting this node
     };
     dfs(root_uid);
 }
